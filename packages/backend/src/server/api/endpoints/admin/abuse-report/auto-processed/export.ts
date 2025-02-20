7c83fadca869d51ccff60562bbd5b5c517b65425
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2025-02-20 20:19:45 +0800

    style

--- packages/backend/src/server/api/endpoints/admin/abuse-report/auto-processed/export.ts
+++ packages/backend/src/server/api/endpoints/admin/abuse-report/auto-processed/export.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { Inject, Injectable } from '@nestjs/common';
 import { format as dateFormat } from 'date-fns';
 import { DI } from '@/di-symbols.js';
