0604f125d61b7c3fa29df3ce6cdf6377f5699f99
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2025-08-25 09:31:13 +0800

    sync

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -1539,7 +1539,7 @@ export interface Locale extends ILocale {
         /**
          * アクティビティを表示する
          */
-        "showActivityiesForVisitor": string;
+        "showActivitiesForVisitor": string;
         "_userGeneratedContentsVisibilityForVisitor": {
             /**
              * 全て公開
