4951f04ac8ca9397693e8ec5e8b9db1a5e57b234
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2025-09-07 19:11:24 +0800

    ?

--- packages/backend/src/models/EmbeddingBatchQueue.ts
+++ packages/backend/src/models/EmbeddingBatchQueue.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { PrimaryColumn, Entity, Column, Index, CreateDateColumn } from 'typeorm';
 import { id } from './util/id.js';
 
