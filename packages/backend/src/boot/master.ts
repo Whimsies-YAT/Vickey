3e2431c7b8edca00551d2eb06dcb32e23d6b4635
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2025-10-31 09:12:30 +0800

    replace deprecated parameter

--- packages/backend/src/boot/master.ts
+++ packages/backend/src/boot/master.ts
@@ -100,7 +100,7 @@ export async function masterMain() {
 			tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
 			// Set sampling rate for profiling - this is relative to tracesSampleRate
-			profilesSampleRate: 1.0,
+			profileSessionSampleRate: 1.0,
 
 			maxBreadcrumbs: 0,
 
