bb8b71d45e209375b1e35bf06143dbd23b2a9300
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2026-03-13 12:27:59 +0800

    fix

--- packages/backend/build.js
+++ packages/backend/build.js
@@ -44,7 +44,7 @@ const externalNativePlugin = {
 /** @type {import('esbuild').BuildOptions} */
 const options = {
 	entryPoints: ['./src/boot/entry.ts'],
-	minify: true,
+	minify: false,
 	keepNames: true,
 	bundle: true,
 	outdir: './built/boot',
